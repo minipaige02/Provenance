4a9f47b7ca6672988da2db586b4e2758998ff43e
Paige Davenport <davenport.paige@outlook.com>
2020-01-05 12:41:26 -0800

    User homepage layout

---
+++ app/src/main/java/minipaige/example/provenance/com/HomeActivity.kt
@@ -0,0 +1,13 @@
+package minipaige.example.provenance.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+}
--- app/src/main/java/minipaige/example/provenance/com/WelcomeActivity.kt
+++ app/src/main/java/minipaige/example/provenance/com/WelcomeActivity.kt
@@ -2,13 +2,11 @@ package minipaige.example.provenance.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.database.FirebaseDatabase
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
     }
-
 }
